30dd45ac813f8d20aa44612036232d97524cd7a7
Sergo1696 <popov19962icloud.com>
2023-02-26 10:32:41 +0500

    second commit

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,7 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 import { VideoModule } from './modules/video.module';
 
 export class ContextMenu extends Menu {
-
+  
+  
 }
--- src/modules/video.module.js
+++ src/modules/video.module.js
@@ -1,5 +1,5 @@
 import { Module } from '../core/module';
-import { createVideo } from '../utils';
+
 
 export class VideoModule extends Module {
   constructor(type, text) {
@@ -7,6 +7,27 @@ export class VideoModule extends Module {
   }
 
   trigger() {
-    createVideo();
+    const container = document.createElement('div');
+   container.className = 'container';
+   container.style.width = '400px';
+   container.style.height = '300px';
+   container.style.position = 'relative';
+   document.body.append(container);
+
+   const deleteBtn = document.createElement('button');
+   deleteBtn.textContent = 'Delete video';
+   deleteBtn.type = 'submit';
+   container.append(deleteBtn);
+   deleteBtn.addEventListener('click', () => {
+       deleteBtn.parentElement.remove();
+   })
+
+   const video = document.createElement('iframe');
+   video.src = "https://www.youtube.com/embed/0bhFkRwjF-U";
+   video.style.position = 'absolute'
+   video.style.width = '100%';
+   video.style.height = '100%';
+   container.append(video);
+   return container;
   }
 }
